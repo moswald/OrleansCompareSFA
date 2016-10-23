cee31ec905651333051cb8a38f5919096d895b61
Matt Oswald <m@oswald.io>
2016-10-23 07:00:49 -0500

    refactored tests to use external storage for test-to-test consistency

--- TestRunnerApi/Controllers/TestsController.cs
+++ TestRunnerApi/Controllers/TestsController.cs
@@ -1,8 +1,8 @@
 ﻿namespace TestRunnerApi.Controllers
 {
     using System;
-    using System.Collections.Generic;
     using System.Diagnostics;
+    using System.Fabric;
     using System.Linq;
     using System.Threading.Tasks;
     using System.Web.Http;
@@ -10,82 +10,120 @@
     using GrainInterfaces;
     using Microsoft.ServiceFabric.Actors;
     using Microsoft.ServiceFabric.Actors.Client;
+    using Microsoft.WindowsAzure.Storage;
+    using Microsoft.WindowsAzure.Storage.Blob;
+    using Newtonsoft.Json;
+    using Newtonsoft.Json.Linq;
     using Orleans;
 
     [RoutePrefix("api/tests")]
     public class TestsController : ApiController
     {
-        const int GrainCount = 10000;
         const int NameLength = 32;
 
         static readonly Random Rng = new Random();
+
+        readonly string _testSetupBlobName;
+        readonly CloudBlobContainer _blobContainer;
+
+        public TestsController()
+        {
+            var appConfig = FabricRuntime.GetActivationContext().GetConfigurationPackageObject("Config");
+            var azureStorageConnectionString = appConfig.Settings.Sections["Storage"].Parameters["AzureStorageConnectionString"].Value;
+            var blobContainerName = appConfig.Settings.Sections["Storage"].Parameters["BlobStorageContainer"].Value;
+            _testSetupBlobName = appConfig.Settings.Sections["Storage"].Parameters["TestSetupBlobName"].Value;
+
+            var storageAccount = CloudStorageAccount.Parse(azureStorageConnectionString);
+            var blobStorageClient = storageAccount.CreateCloudBlobClient();
+            _blobContainer = blobStorageClient.GetContainerReference(blobContainerName);
+        }
+
+        [HttpGet]
+        [Route("setup")]
+        public async Task<IHttpActionResult> Setup(int count, int extraDataSize = 0)
+        {
+            await _blobContainer.DeleteIfExistsAsync().ConfigureAwait(false);
+            await _blobContainer.CreateAsync().ConfigureAwait(false);
+            await _blobContainer.SetPermissionsAsync(new BlobContainerPermissions { PublicAccess = BlobContainerPublicAccessType.Container }).ConfigureAwait(false);
+
+            var ids = Enumerable.Range(0, count).Select(_ => Guid.NewGuid()).ToArray();
+            var names = Enumerable.Range(0, count * 2).Select(_ => RandomString(NameLength)).ToArray();
+
+            var jsonValue = JObject
+                .FromObject(
+                    new
+                    {
+                        count,
+                        ids,
+                        names,
+                        extraDataSize
+                    })
+                .ToString(Formatting.Indented);
+
+            var blob = _blobContainer.GetBlockBlobReference(_testSetupBlobName);
+            await blob.UploadTextAsync(jsonValue).ConfigureAwait(false);
+
+            return Ok();
+        }
 
         [HttpGet]
         [Route("initializeOrleans/")]
         public async Task<IHttpActionResult> InitializeOrleans()
         {
-            var grainIds = Enumerable.Range(0, GrainCount).Select(_ => Guid.NewGuid()).ToArray();
-            var grainNames = Enumerable.Range(0, GrainCount * 2).Select(_ => RandomString(NameLength)).ToArray();
+            var result = await Initialize(
+                (grainId, bestFriendId, firstName, lastName, extraDataSize) =>
+                {
+                    var grain = GrainClient.GrainFactory.GetGrain<IFriendlyGrain>(grainId);
+                    return grain.Initialize(bestFriendId, firstName, lastName, extraDataSize);
+                });
 
-            var initializationCalls = new Task[GrainCount];
-
-            var sw = Stopwatch.StartNew();
-
-            for (var i = 0; i != GrainCount; ++i)
-            {
-                var grain = GrainClient.GrainFactory.GetGrain<IFriendlyGrain>(grainIds[i]);
-                var bestFriend = i + 1 == GrainCount ? 0 : i + 1;
-
-                initializationCalls[i] = grain.Initialize(grainIds[bestFriend], grainNames[i * 2], grainNames[i * 2 + 1], 0);
-            }
-
-            await Task.WhenAll(initializationCalls);
-
-            sw.Stop();
-
-            return Ok(sw.Elapsed);
+            return Ok(result);
         }
 
         [HttpGet]
         [Route("initializeActors/")]
         public async Task<IHttpActionResult> InitializeActors()
         {
-            var actorIds = Enumerable.Range(0, GrainCount).Select(_ => ActorId.CreateRandom()).ToArray();
-            var actorNames = Enumerable.Range(0, GrainCount * 2).Select(_ => RandomString(NameLength)).ToArray();
+            var result = await Initialize(
+                (actorId, bestFriendId, firstName, lastName, extraDataSize) =>
+                {
+                    var actor = ActorProxy.Create<IFriendlyActor>(new ActorId(actorId));
+                    return actor.Initialize(new ActorId(bestFriendId), firstName, lastName, extraDataSize);
+                });
 
-            var initializationCalls = new Task[GrainCount];
+            return Ok(result);
+        }
+
+        async Task<TimeSpan> Initialize(Func<Guid, Guid, string, string, int, Task> create)
+        {
+            var blob = _blobContainer.GetBlockBlobReference(_testSetupBlobName);
+            var jsonValue = await blob.DownloadTextAsync().ConfigureAwait(false);
+
+            var json = JObject.Parse(jsonValue);
+
+            var count = json["count"].Value<int>();
+            var ids = json["ids"].Values<string>().Select(Guid.Parse).ToArray();
+            var names = json["names"].Values<string>().ToArray();
+            var extraDataSize = json["extraDataSize"].Value<int>();
+
+            var initializationCalls = new Task[count];
 
             var sw = Stopwatch.StartNew();
 
-            for (var i = 0; i != GrainCount; ++i)
+            for (var i = 0; i != count; ++i)
             {
-                var grain = ActorProxy.Create<IFriendlyActor>(actorIds[i]);
-                var bestFriend = i + 1 == GrainCount ? 0 : i + 1;
+                var bestFriend = i + 1 == count ? 0 : i + 1;
 
-                initializationCalls[i] = grain.Initialize(actorIds[bestFriend], actorNames[i * 2], actorNames[i * 2 + 1], 0);
+                initializationCalls[i] = create(ids[i], ids[bestFriend], names[i * 2], names[i * 2 + 1], extraDataSize);
             }
 
             await Task.WhenAll(initializationCalls);
 
             sw.Stop();
 
-            return Ok(sw.Elapsed);
+            return sw.Elapsed;
         }
 
         static string RandomString(int length) => new string(Enumerable.Range(0, length).Select(c => (char)Rng.Next('A', 'Z' + 1)).ToArray());
-
-        static IEnumerable<Tuple<T, T>> ToPairs<T>(IEnumerable<T> sequence)
-        {
-            using (var e = sequence.GetEnumerator())
-            {
-                while (e.MoveNext())
-                {
-                    var first = e.Current;
-                    e.MoveNext();
-
-                    yield return Tuple.Create(first, e.Current);
-                }
-            }
-        }
     }
 }
